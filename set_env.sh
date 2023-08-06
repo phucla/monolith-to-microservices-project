34cbe81cd0ac547af9ccec5ba46af38055d61108
PhucLa <laducphuc@gmail.com>
2023-08-06 21:18:29 +0700

    Add screenshots

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=laducphuc
-export POSTGRES_HOST=database-1.cbyjfv97wriv.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=http://localhost:8000
 export POSTGRES_DB=postgres
-export AWS_BUCKET=my-bucket-demo-project2
+export AWS_BUCKET=aws-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
